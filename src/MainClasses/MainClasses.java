9e7d96828af37a59c691cce883690da3392d2ca3
Pedro Gonzalez <pedro.jgonzalez73@gmail.com>
2025-02-07 21:26:54 -0400

    Main

--- src/MainClasses/MainClasses.java
+++ src/MainClasses/MainClasses.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Helpers;
+package MainClasses;
 
 /**
  *
  * @author pedro
  */
-public class HelpersFunctions {
+public class MainClasses {
     
 }
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -10,7 +10,6 @@ package MainPackage;
  */
 public class Main {
      public static void main(String[] args) {
-        //App app = App.getInstance();
-        //app.start();
+        
     }
 }
